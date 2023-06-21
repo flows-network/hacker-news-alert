7f029d20ebd9374bff5877a2d73cf463b1e1b1be
Ji Chen <jaykchen@gmail.com>
2023-06-21 09:53:26 -0400

    @55

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ use web_scraper_flows::get_page_text;
 pub fn run() {
     dotenv().ok();
     let keyword = std::env::var("KEYWORD").unwrap_or("chatGPT".to_string());
-    schedule_cron_job(String::from("49 * * * *"), keyword, callback);
+    schedule_cron_job(String::from("55 * * * *"), keyword, callback);
 }
 
 #[no_mangle]
@@ -127,6 +127,9 @@ async fn get_summary_truncated(inp: &str) -> anyhow::Result<String> {
     match openai.chat_completion(&chat_id, &question, &co).await {
         Ok(r) => {
             let text = r.choice;
+            let head = text.chars().take(150).collect::<String>();
+            send_message_to_channel(&workspace, "ch_err", head).await;
+
             let start_index = text.find('{').unwrap();
             let end_index = text.rfind('}').unwrap();
             let json_string = &text[start_index..=end_index];
@@ -144,4 +147,14 @@ struct Article {
     summary: String,
     keywords: Vec<String>,
     word_count: i32,
+}
+
+impl Default for Article {
+    fn default() -> Self {
+        Self {
+            summary: "failed to parse gpt returned summary".to_string(),
+            keywords: Vec::new(),
+            word_count: 0,
+        }
+    }
 }
